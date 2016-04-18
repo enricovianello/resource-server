9e52004cb438fd0b488bfafbc2eeb1d6baf45549
enricovianello <maverick17586@gmail.com>
2016-04-18 17:44:31 +0200

    Fixes to make it works by running spring-boot:run
    - Fixed V2 migration script
    - Added configuration properties file
    
    Extra:
    - SpringBoot version moved to 1.3.2

--- src/main/resources/db/migration/mysql/V2__change_naming_strategy.sql
+++ src/main/resources/db/migration/mysql/V2__change_naming_strategy.sql
@@ -2,21 +2,31 @@ ALTER TABLE scim_address CHANGE streetaddress street_address varchar(255) DEFAUL
 
 ALTER TABLE scim_address CHANGE postalcode postal_code varchar(255) DEFAULT NULL;
 
+ALTER TABLE scim_extension_field DROP FOREIGN KEY fk_eksek96tmtxkaqe5a7hfmoswo;
 ALTER TABLE scim_extension_field CHANGE extension_internal_id extension bigint(20) NOT NULL;
+ALTER TABLE scim_extension_field ADD CONSTRAINT fk_eksek96tmtxkaqe5a7hfmoswo FOREIGN KEY (extension) REFERENCES scim_extension (internal_id);
 
 ALTER TABLE scim_extension_field CHANGE is_required required tinyint(1) DEFAULT NULL;
 
+ALTER TABLE scim_extension_field_value DROP FOREIGN KEY fk_6y0v7g2y69nkvody9jv5q3tuo;
 ALTER TABLE scim_extension_field_value CHANGE extension_field_internal_id extension_field bigint(20) NOT NULL;
+ALTER TABLE scim_extension_field_value ADD CONSTRAINT fk_6y0v7g2y69nkvody9jv5q3tuo FOREIGN KEY (extension_field) REFERENCES scim_extension_field (internal_id);
 
 ALTER TABLE scim_group CHANGE displayname display_name varchar(255) NOT NULL;
 
+ALTER TABLE scim_group_scim_id DROP FOREIGN KEY fk_gct22972jrrv22crorixfdlmi;
 ALTER TABLE scim_group_scim_id CHANGE groups_internal_id groups bigint(20) NOT NULL;
+ALTER TABLE scim_group_scim_id ADD CONSTRAINT fk_gct22972jrrv22crorixfdlmi FOREIGN KEY (groups) REFERENCES scim_group (internal_id);
 
+ALTER TABLE scim_group_scim_id DROP FOREIGN KEY fk_b29y2qc2j5uu49wa9grpbulb0;
 ALTER TABLE scim_group_scim_id CHANGE members_internal_id  members bigint(20) NOT NULL;
+ALTER TABLE scim_group_scim_id ADD CONSTRAINT fk_b29y2qc2j5uu49wa9grpbulb0 FOREIGN KEY (members) REFERENCES scim_id (internal_id);
 
 ALTER TABLE scim_group_scim_id RENAME TO scim_group_members;
 
+ALTER TABLE scim_id DROP FOREIGN KEY fk_byxttqfbmb2wcj4ud3hd53mw3;
 ALTER TABLE scim_id CHANGE meta_id meta bigint(20) NOT NULL;
+ALTER TABLE scim_id ADD CONSTRAINT fk_byxttqfbmb2wcj4ud3hd53mw3 FOREIGN KEY (meta) REFERENCES scim_meta (id);
 
 ALTER TABLE scim_meta CHANGE lastmodified last_modified timestamp NULL DEFAULT NULL;
 
@@ -44,4 +54,6 @@ ALTER TABLE scim_user CHANGE preferredlanguage preferred_language varchar(255) D
 
 ALTER TABLE scim_user CHANGE displayname display_name varchar(255) DEFAULT NULL;
 
+ALTER TABLE scim_user DROP FOREIGN KEY fk_d2ji7ipe62fbg8uu2ir7b9ls4;
 ALTER TABLE scim_user CHANGE name_id name bigint(20) DEFAULT NULL;
+ALTER TABLE scim_user ADD CONSTRAINT fk_d2ji7ipe62fbg8uu2ir7b9ls4 FOREIGN KEY (name) REFERENCES scim_name (id);
